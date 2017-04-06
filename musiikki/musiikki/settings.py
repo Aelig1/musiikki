dec718492db701e522b93888687da24cdf42c847
Aapo Hakamaa <aapo.hakamaa@student.tut.fi>
2017-04-07 02:10:43 +0300

    Static dirs modified

--- musiikki/musiikki/settings.py
+++ musiikki/musiikki/settings.py
@@ -117,4 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
