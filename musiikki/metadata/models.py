16264855be61b285f46fd25d00daea81e2d05361
Aapo Hakamaa <aapo.hakamaa@student.tut.fi>
2017-04-07 19:12:58 +0300

    Metadata app added with models defined

---
+++ musiikki/metadata/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    
+    class Meta:
+        ordering = ['name']
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums')
+    year = models.IntegerField()
+    
+    class Meta:
+        ordering = ['year', 'name']
+
+class Track(models.Model):
+    name = models.CharField(max_lenght=255)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='tracklist')
+    duration = models.DurationField()
+    
+    class Meta:
+        ordering = ['name']
