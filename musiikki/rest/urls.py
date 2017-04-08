735af462374b39ba7e7575a2146e8b4f013571bb
Aapo Hakamaa <aapo.hakamaa@student.tut.fi>
2017-04-08 19:07:06 +0300

    In rest app different view methods added for artists, albums and tracks. Inside these views http method is inspected. Only GET method is implemented.

--- musiikki/rest/urls.py
+++ musiikki/rest/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.ui),
-	url(r'^modify$', views.modify),
+	url(r'^artist/(\d+)$', views.artist),
+	url(r'^artists$', views.listArtists),
+	url(r'^album/(\d+)$', views.album),
+	url(r'^track/(\d+)$', views.track),
 	url(r'^search$', views.search)
 ]
--- musiikki/rest/views.py
+++ musiikki/rest/views.py
@@ -1,28 +1,127 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from django.core import serializers
 import json
+
+import datetime
 
 from metadata.models import Artist, Album, Track
 
 def ui(request):
 	return render(request, 'ui.html')
 
-def modify(request):
+def artist(request, id):
+	# Find artist
+	try:
+		artist = Artist.objects.get(id=id)
+	except Artist.DoesNotExist:
+		raise Http404('Artist with ID ' + id + ' does not exist.')
 	
-	return HttpResponse("modify", content_type='application/json')
+	# Examine request
+	if request.method == 'GET':
+		# Check callback
+		callback = request.GET.get('callback')
+		if (callback != None):
+			# Stringify aritst inside callback function call
+			json_data = callback + '(' + json.dumps(artist.dict()) + ');'
+		else:
+			# No callback, stringify with indentation
+			json_data = json.dumps(artist.dict(), indent=2)
+		
+		return HttpResponse(json_data, content_type='application/json')
+	
+	elif request.method == 'PUT':
+		pass
+	
+	elif request.method == 'DELETE':
+		pass
+	
+	return HttpResponse(id, content_type='application/json')
 
-def search(request):
-	# Artist
-	artist_id = request.GET.get('artist_id')
-	print(artist_id)
-	artist = request.GET.get('artist')
-	genre = request.GET.get('genre')
-	# Album
-	album_id = request.GET.get('album_id')
-	album = request.GET.get('album')
-	year = request.GET.get('year')	
-	# Track
-	track_id = request.GET.get('track_id')
-	track = request.GET.get('track')
+def listArtists(request):
+	artists = []
+	for artist in Artist.objects.all():
+		artists.append(artist.dict())
 	
-	return HttpResponse("search JSON", content_type='application/json')
+		# Check callback
+		callback = request.GET.get('callback')
+		if (callback != None):
+			# Stringify aritsts inside callback function call
+			json_data = callback + '(' + json.dumps(artists) + ');'
+		else:
+			# No callback, stringify with indentation
+			json_data = json.dumps(artists, indent=2)
+		
+	return HttpResponse(json_data, content_type='application/json')
+
+def album(request, id):
+	# Find album
+	try:
+		album = Album.objects.get(id=id)
+	except Album.DoesNotExist:
+		raise Http404('Album with ID ' + id + ' does not exist.')
+	
+	# Examine request
+	if request.method == 'GET':
+		# Check callback
+		callback = request.GET.get('callback')
+		if (callback != None):
+			# Stringify album inside callback function call
+			json_data = callback + '(' + json.dumps(album.dict()) + ');'
+		else:
+			# No callback, stringify with indentation
+			json_data = json.dumps(album.dict(), indent=2)
+		
+		return HttpResponse(json_data, content_type='application/json')
+	
+	elif request.method == 'PUT':
+		pass
+	
+	elif request.method == 'DELETE':
+		pass
+	
+	return HttpResponse(id, content_type='application/json')
+
+def track(request, id):
+	# Find track
+	try:
+		track = Track.objects.get(id=id)
+	except Track.DoesNotExist:
+		raise Http404('Track with ID ' + id + ' does not exist.')
+	
+	# Examine request
+	if request.method == 'GET':
+		# Check callback
+		callback = request.GET.get('callback')
+		if (callback != None):
+			# Stringify track inside callback function call
+			json_data = callback + '(' + json.dumps(track.dict()) + ');'
+		else:
+			# No callback, stringify with indentation
+			json_data = json.dumps(track.dict(), indent=2)
+		
+		return HttpResponse(json_data, content_type='application/json')
+	
+	elif request.method == 'PUT':
+		pass
+	
+	elif request.method == 'DELETE':
+		pass
+	
+	return HttpResponse(id, content_type='application/json')
+	
+def search(request):
+	return JSONResponse(request.GET.dict())
+
+def JSONResponse(dict):
+	'''
+	try:
+		artist_id = dict['artist_id']
+	    try:
+	        artist = Artist.objects.get(code=artist_id)
+	    except Artist.DoesNotExist:
+	        raise Http404('Continent \"' + art + '\" does not exist')
+	except KeyError:
+		pass
+	'''
+	return HttpResponse("JSON", content_type='application/json')
