95fb95fd379b9ab734611df1ae4a373cc82d5bd4
Aapo Hakamaa <aapo.hakamaa@student.tut.fi>
2017-04-08 12:25:53 +0300

    Root page redirects to api

--- musiikki/musiikki/urls.py
+++ musiikki/musiikki/urls.py
@@ -1,11 +1,12 @@
 # musiikki URL Configuration
-
 from django.conf.urls import include, url
+from django.views.generic import RedirectView
 from django.contrib import admin
 
 from rest import urls as rest_urls
 
 urlpatterns = [
+	url(r'^$', RedirectView.as_view(url='api')),
 	url(r'^admin/', admin.site.urls),
 	url(r'^api/', include(rest_urls))
 ]
