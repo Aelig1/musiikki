5654fa15a895ad71f5805d5be036f1d3d458e257
Aapo Hakamaa <aapo.hakamaa@student.tut.fi>
2017-04-08 03:50:06 +0300

    Template added for api ui

--- musiikki/rest/urls.py
+++ musiikki/rest/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^add$', views.add),
+	url(r'^$', views.ui),
 	url(r'^modify$', views.modify),
-	url(r'^remove$', views.remove),
-    url(r'^search$', views.search)
+	url(r'^search$', views.search)
 ]
--- musiikki/rest/views.py
+++ musiikki/rest/views.py
@@ -1,16 +1,28 @@
+from django.shortcuts import render
 from django.http import HttpResponse, Http404
 import json
 
 from metadata.models import Artist, Album, Track
 
-def add(request):
-	return HttpResponse("add", content_type='application/json')
+def ui(request):
+	return render(request, 'ui.html')
 
 def modify(request):
+	
 	return HttpResponse("modify", content_type='application/json')
 
-def remove(request):
-	return HttpResponse("remove", content_type='application/json')
-
 def search(request):
+	# Artist
+	artist_id = request.GET.get('artist_id')
+	print(artist_id)
+	artist = request.GET.get('artist')
+	genre = request.GET.get('genre')
+	# Album
+	album_id = request.GET.get('album_id')
+	album = request.GET.get('album')
+	year = request.GET.get('year')	
+	# Track
+	track_id = request.GET.get('track_id')
+	track = request.GET.get('track')
+	
 	return HttpResponse("search JSON", content_type='application/json')
